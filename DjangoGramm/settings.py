305c11d62515a45abf21b6dfa46f625eb425dd76
kir <kirillbogatuk2005@gmail.com>
2024-03-07 21:34:19 +0200

    Still trying

--- DjangoGramm/settings.py
+++ DjangoGramm/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'AppDjangoGramm.apps.AppdjangogrammConfig',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
